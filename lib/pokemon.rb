bed415aaa70a41bab6ca608f09d625f561ca7436
Yuliana Alvarado <yalvarado924@gmail.com>
2021-01-14 15:48:39 -0600

    commit tree b38899422caea13cbca5629ac9fc16e8ce8006f7

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+
+    def self.find(id_number, db)
+        pokemon_data = db.execute("SELECT * FROM pokemon WHERE id = ?", id_number).flatten
+        Pokemon.new(id: pokemon_data[0], name: pokemon_data[1], type: pokemon_data[2], db: db)
+
+    end
 end
